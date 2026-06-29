a0a8747dc84768e40f73175f1439efc335a2f536
yerald0108 <yeraldfuste@gmail.com>
2026-06-29 16:18:27 -0400

    fix: use reanimated 4.1 compatible with expo SDK 54 RN 0.81

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,6 @@
-// babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    // ← NO debe haber plugins: ['react-native-reanimated/plugin'] aquí
   };
 };
